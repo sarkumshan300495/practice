15e7600b055f56114ef8360a7cf0299b2aff0293
sarkumshan300495 <saran30kumar95@gmail.com>
2023-08-26 21:08:04 +0530

    Initial commit

---
+++ bin/practice.dart
@@ -0,0 +1,25 @@
+bool isPrime(double number) {
+  if (number <= 1) {
+    return false; // Numbers less than or equal to 1 are not prime
+  }
+  
+  for (double i = 2; i <= number / 2; i++) {
+    
+    if (number % i == 0) {
+      
+      return false; // If the number is divisible by any i, it's not prime
+    }
+  }
+  
+  return true; // If no divisors are found, the number is prime
+}
+
+void main() {
+  double num = 18;
+  
+  if (isPrime(num)) {
+    print('$num is a prime number');
+  } else {
+    print('$num is not a prime number');
+  }
+}
